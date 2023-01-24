00af532154cd46c753f8ca022a449f578fa09283
North-tx <120264083+North-tx@users.noreply.github.com>
2023-01-24 20:45:26 +0300

    Initilal Commit

---
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -0,0 +1,15 @@
+package ru.netology.sqr;
+
+public class SQRService {
+    public int calc (int minLimit, int maxLimit){
+        int count = 0;
+        for (int i = 10; i <=99 ; i++) {
+            if (i*i >= minLimit){
+                if (i*i <= maxLimit){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
